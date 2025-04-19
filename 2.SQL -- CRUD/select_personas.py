64dc17e28c7f0527f3d52c37958c08c8e7270cf1
Desiree05 <95355425+Desiree05@users.noreply.github.com>
2025-04-19 12:29:35 +0100

    Update select_personas.py

--- 2.SQL -- CRUD/select_personas.py
+++ 2.SQL -- CRUD/select_personas.py
@@ -2,14 +2,12 @@ import mysql.connector;
 
 conexion = mysql.connector.connect(
     host="localhost", # 127.0.0.1
-    port="3366",
     user="root",
     password="admin",
     database="mydb",
 )
 
 cursor = conexion.cursor()
-
 
 consulta_SELECT = "SELECT * FROM personas;"
 cursor.execute(consulta_SELECT)
@@ -19,8 +17,5 @@ resultados = cursor.fetchall()
 for fila in resultados:
     print(fila)
 
-
 cursor.close()
 conexion.close()
-
-
