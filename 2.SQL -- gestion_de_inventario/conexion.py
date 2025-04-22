eb60537d956ab39964fe15a15e3742a1b6236389
Desiree05 <95355425+Desiree05@users.noreply.github.com>
2025-04-22 22:05:04 +0100

    Conectado con la BBDD
    Faltaría crear el servicio y el modelo interfaz con el que el usuario va a interactuar.

---
+++ 2.SQL -- gestion_de_inventario/conexion.py
@@ -0,0 +1,63 @@
+# Clase para crear el pool de conexiones
+
+from mysql.connector import pooling
+from mysql.connector import Error
+
+class Conexion:
+    DATABASE = "almacen_db"
+    USERNAME = "root"
+    PASSWORD = "algoSQL_05"
+    DB_PORT = "3366"
+    HOST = "localhost"
+    POOL_SIZE = 5
+    POOL_NAME = "almacen_pool"
+    pool = None
+
+    @classmethod
+    def obtener_pool(cls):  #cls = como self.Conexion
+        if cls.pool is None:
+            try:
+                cls.pool = pooling.MySQLConnectionPool(
+                    pool_name= cls.POOL_NAME,
+                    pool_size= cls.POOL_SIZE,
+                    host= cls.HOST,
+                    port= cls.DB_PORT,
+                    database=cls.DATABASE,
+                    user= cls.USERNAME,
+                    password= cls.PASSWORD
+                )
+
+                return cls.pool
+            except Error as e:
+                print(f"Ocurrio un error al obtener el pool de conexiones: {e}")
+            
+        else:
+            return cls.pool
+        
+
+    
+    # Lo que establece conexion al usuario    
+    @classmethod
+    def obtener_conexion(cls):
+        return cls.obtener_pool().get_connection()
+    
+
+    # Lo que rompe conexion al usuario    
+    @classmethod
+    def liberar_conexion(cls, conexion):
+        conexion.close()
+
+
+"""""
+if __name__ == '__main__':
+    pool = Conexion.obtener_pool()
+    print(pool)
+
+    conexion1 = pool.get_connection()
+    print(conexion1)
+
+    Conexion.liberar_conexion(conexion1)
+
+    print("Se ha liberado el objeto conexion1")
+
+"""""
