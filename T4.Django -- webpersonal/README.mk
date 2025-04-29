10e34b44dd08eb24f5538bdf93476a391b367c95
Desiree05 <95355425+Desiree05@users.noreply.github.com>
2025-04-29 13:10:51 +0100

    Initial commit

---
+++ T4.Django -- webpersonal/README.mk
@@ -0,0 +1,7 @@
+# Install the following
+- pipenv install django django-ckeditor Pillow pylint pylint-django pylint-celery
+- pipenv shell # para activar el entornor
+
+# Posibles comandos para ejecutarlo:
+- python manage.py runserver
+- pipenv run webpersonal
