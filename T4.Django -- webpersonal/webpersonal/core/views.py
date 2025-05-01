0063b226ccf554b5346b6366e2ef10a44cb4de54
Desiree05 <95355425+Desiree05@users.noreply.github.com>
2025-05-01 19:56:38 +0100

    Adding template tags
    Making it less redundant

--- T4.Django -- webpersonal/webpersonal/core/views.py
+++ T4.Django -- webpersonal/webpersonal/core/views.py
@@ -1,37 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-<h1>¡Bienvenido!</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about/">Acerca de...</a></li>
-    <li><a href="/portfolio/">Portfolio</a></li>
-    <li><a href="/contacto/">Contacto</a></li>
-</ul>
-"""
-
 # Create your views here.
 def home(request):
-    return HttpResponse(html_base + """
-        <h2>Portada</h2>
-        <p>Esto es la portada</p>
-        """)
-
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse(html_base + """
-        <h2>Acerca de...</h2>
-        <p>Me llamo Victoria y soy programadora.</p>
-        """)
+    return render(request, "core/about.html")
 
 def portfolio(request):
-    return HttpResponse(html_base + """
-        <h2>Portfolio</h2>
-        <h3>Mis proyectos:</h3>
-        """)
+    return render(request, "core/portfolio.html")
 
 def contacto(request):
-    return HttpResponse(html_base + """
-        <h2>Contacto</h2>
-        <p>Aquí os dejo mi email: desimarce05@gmail.com</p>
-        """)
+    return render(request, "core/contacto.html")
